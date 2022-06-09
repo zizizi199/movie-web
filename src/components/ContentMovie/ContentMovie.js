7f999b305f222b8d4bc12e5d4280b366d5e6b9c7
zizizi199 <noroiandcat@gmail.com>
2022-06-09 16:59:59 +0700

    update

--- src/components/ContentMovie/ContentMovie.js
+++ src/components/ContentMovie/ContentMovie.js
@@ -3,16 +3,16 @@ import './ContentMovie.css'
 const IMG_API = 'https://image.tmdb.org/t/p/w500/';
 
 
-function ContentMovie({ title, name, media_type, poster_path, vote_average, first_air_date, release_date }) {
+function ContentMovie({ ...movie}) {
     return (
         <div className='movie'>
-            <img src={poster_path?IMG_API + poster_path : 'https://www.movienewz.com/img/films/poster-holder.jpg' } alt={name} />
+            <img src={movie.poster_path?IMG_API + movie.poster_path : 'https://www.movienewz.com/img/films/poster-holder.jpg' } alt={movie.name} />
             <div className='text-container'>
-                <h3>{name || title}</h3>
-                <span className='description'>{media_type}
-                    <span>{vote_average}</span>
+                <h3>{movie.name || movie.title}</h3>
+                <span className='description'>{movie.media_type}
+                    <span>{movie.vote_average}</span>
                 </span>
-                <div className='date'>{first_air_date || release_date}</div>
+                <div className='date'>{movie.first_air_date || movie.release_date}</div>
             </div>
         </div>
     )
